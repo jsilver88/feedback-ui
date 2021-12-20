ce9eef0c801de372e9ff24bdda1a5130441b91ee
Jared Silver <jrsilver88@gmail.com>
2021-12-20 13:47:22 -0800

    context api

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,29 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tenetur, unde qui modi laboriosam suscipit accusantium vitae eius consectetur blanditiis sequi maxime, odit placeat illum, voluptas quaerat inventore molestias asperiores?',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tenetur, unde qui modi laboriosam suscipit accusantium vitae eius consectetur blanditiis sequi maxime, odit placeat illum, voluptas quaerat inventore molestias asperiores?',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tenetur, unde qui modi laboriosam suscipit accusantium vitae eius consectetur blanditiis sequi maxime, odit placeat illum, voluptas quaerat inventore molestias asperiores?',
+  },
+  {
+    id: 4,
+    rating: 7,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tenetur, unde qui modi laboriosam suscipit accusantium vitae eius consectetur blanditiis sequi maxime, odit placeat illum, voluptas quaerat inventore molestias asperiores?',
+  },
+  {
+    id: 5,
+    rating: 6,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tenetur, unde qui modi laboriosam suscipit accusantium vitae eius consectetur blanditiis sequi maxime, odit placeat illum, voluptas quaerat inventore molestias asperiores?',
+  },
+]
+
+export default FeedbackData
